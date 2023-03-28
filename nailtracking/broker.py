ab74e7babff9f9c009b8a954a7e978456ce6cef6
thermeto <vladyslav.oliinyk94@gmail.com>
2023-03-28 21:14:17 +0300

    working prod-cons pipe

--- nailtracking/broker.py
+++ nailtracking/broker.py
@@ -11,23 +11,41 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+
 # Function to handle incoming messages
 def on_message(channel, method_frame, header_frame, body):
-    logging.info('Received photo')
-    output_folder = '/nailtracking_images/output_images'
+    try:
+        logging.info('Received photo')
+        output_folder = '/nailtracking_images/output_images'
 
-    image = cv2.imdecode(np.frombuffer(body, np.uint8), cv2.IMREAD_UNCHANGED)
+        image = cv2.imdecode(np.frombuffer(body, np.uint8), cv2.IMREAD_UNCHANGED)
 
-    start_time = time.time()
-    # Process the image and get the output path
-    output_image = infer_photo.transparentize_nails(image)
-    elapsed_time = time.time() - start_time
-    logging.info(f"Elapsed time for nailstracking: {elapsed_time:.2f} seconds")
+        start_time = time.time()
+        # Process the image and get the output path
+        output_image = infer_photo.transparentize_nails(image)
+        elapsed_time = time.time() - start_time
+        logging.info(f"Elapsed time for nailstracking: {elapsed_time:.2f} seconds")
 
-    output_image_path = os.path.join(output_folder, 'output_image.png')
-    cv2.imwrite(output_image_path, output_image)
-    logging.info(f"Photo {output_image_path} successfully saved")
+        output_image_path = os.path.join(output_folder, 'output_image.png')
+        cv2.imwrite(output_image_path, output_image)
+        logging.info(f"Photo {output_image_path} successfully saved")
 
+        # Encode the output image as a base64 string
+        _, output_buffer = cv2.imencode('.png', output_image)
+        modified_image_base64 = base64.b64encode(output_buffer).decode('utf-8')
+
+    except Exception as e:
+        logging.error(f"Unexpected error: {e}")
+        modified_image_base64 = f"error: {e}"
+
+    # Send the result (modified image or error) back to the producer
+    channel.basic_publish(exchange='',
+                          routing_key=header_frame.reply_to,
+                          properties=pika.BasicProperties(correlation_id=header_frame.correlation_id),
+                          body=modified_image_base64)
+
+    # Acknowledge the message so it is removed from the nailtracking_queue
+    channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
 
 # Connect to RabbitMQ
